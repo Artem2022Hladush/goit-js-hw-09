1556eabab7ff90b4df0d7b3d61a341db85cdb4a4
Артем Гладуш <m1hommage5008@gmail.com>
2022-09-24 14:28:22 +0300

    add code colr-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+btnStart: document.querySelector('[data-start]'),
+btnStop: document.querySelector('[data-stop]'),
+body: document.body,
+};
 
+let isActive = false;
+let intervalId = null;
+
+
+onBtnStopClick();
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() { 
+   refs.btnStart.disabled = !isActive;
+   refs.btnStop.disabled = isActive;
+
+	intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+      console.log(refs.body.style.backgroundColor = getRandomHexColor());
+   }, 1000);
+};
+
+function onBtnStopClick(e) {
+   clearInterval(intervalId);
+
+   refs.btnStop.disabled = !isActive;
+   refs.btnStart.disabled = isActive;
+};
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
